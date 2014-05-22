7c5baa9f8fd90f87b5ca5e51ce8d86441d87d790
Camille Reynders <camille.reynders@gmail.com>
2014-05-22 16:15:37 +0200

    See #3, set up first testing function

--- test/functions.spec.js
+++ test/functions.spec.js
@@ -1,6 +1,7 @@
 /* jshint -W024, expr:true */
 /*global expect, fx, sinon*/
 'use strict';
+var statutils = require( '..' ).functions;
 
 describe( 'functions', function(){
 
@@ -8,12 +9,19 @@ describe( 'functions', function(){
         it( 'should pass', function(){
             expect( true ).to.equal( true );
         } );
-        it('should have access to the fixtures', function(){
-            expect(fx.decisions ).to.not.be.undefined(); 
-            expect(fx.judges ).to.not.be.undefined(); 
-            expect(fx.players ).to.not.be.undefined(); 
-            expect(fx.tasks).to.not.be.undefined(); 
-        });
+        it( 'should have access to the fixtures', function(){
+            expect( fx.decisions ).to.not.be.undefined();
+            expect( fx.judges ).to.not.be.undefined();
+            expect( fx.players ).to.not.be.undefined();
+            expect( fx.tasks ).to.not.be.undefined();
+        } );
+    } );
+
+    describe( 'rasch', function(){
+        var rasch = statutils.rasch;
+        it( 'should return 0.5 when the ability equals the difficulty', function(){
+            expect( rasch( 0, 0 ) ).to.equal( .5 );
+        } );
     } );
 
 } );
