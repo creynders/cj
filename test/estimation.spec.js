52c428e824d3111d64deadf38c9629e1758714b4
Brian Henderson <BrianSHenderson@gmail.com>
2014-05-28 12:25:48 +0000

    judge_infit_rewrite

--- test/estimation.spec.js
+++ test/estimation.spec.js
@@ -2,6 +2,7 @@
 /*global expect, fx, sinon*/
 'use strict';
 var estimation = require( '..' ).estimation;
+var statutils = require( '..' ).statutils;
 
 describe( 'estimation', function(){
 
@@ -25,4 +26,66 @@ describe( 'estimation', function(){
         } );
     });
   
-} );
+    describe('probabilities', function(){
+      var probabilities = estimation.probabilities;
+      var player1 = new Object(); 
+      player1.trueScore = 3;
+      player1._id = 1
+      var player2 = new Object(); 
+      player2.trueScore = 3;
+      player2._id = 2
+      var players = [player1,player2];
+      var decision = new Object();
+      decision.chosen = player1._id;
+      decision.notChosen = player2._id;
+      var decisions = [decision];
+      it( 'should return probabilities', function(){
+            expect( probabilities( [] ,[]).length ).to.equal( 0 );            
+            expect( probabilities( players , decisions )[0]).to.equal( 0.5 );
+            player2.trueScore = -1;
+            expect( probabilities( players , decisions )[0]).to.be.above( 0.5 );
+            player2.trueScore = 4;
+            expect( probabilities( players , decisions )[0]).to.be.below( 0.5 );
+            expect( probabilities( fx.players , fx.decisions ).length).to.equal(fx.decisions.length);
+            var probs = probabilities(fx.players , fx.decisions);
+            expect(statutils.wms(probs)).to.be.between(0.9,1.1);
+        } );
+    });
+  
+    describe('judgeProbs', function(){
+      var probabilities = estimation.probabilities;
+      var judgeProbs = estimation.judgeProbs;
+      var getJudgeStats = estimation.getJudgeStats;
+      var player1 = new Object(); 
+      player1.trueScore = 3;
+      player1._id = 1
+      var player2 = new Object(); 
+      player2.trueScore = 3;
+      player2._id = 2
+      var players = [player1,player2];
+      var decision = new Object();
+      decision.chosen = player1._id;
+      decision.notChosen = player2._id;
+      var judge1 = new Object();
+      judge1._id = 123;
+      decision.judge = judge1._id;
+      var judge2 = new Object();
+      judge2._id = 124;
+      var decisions = [decision];
+      var judges = [judge1,judge2];
+      it( 'should return judges probs', function(){
+        var probs = probabilities(players , decisions);
+        expect(judgeProbs(judge1,probs,decisions)[0]).to.equal(0.5);
+        expect(judgeProbs(judge2,probs,decisions).length).to.equal(0);
+        expect(getJudgeStats(judges, probs, decisions)).to.eql([1,0]);
+        probs = probabilities(fx.players , fx.decisions);
+        var infit = getJudgeStats(fx.judges, probs, fx.decisions);
+        for (var i=0; i<infit.length; i++){
+          if(fx.judges[i].hasOwnProperty('trueScore')){
+            expect(fx.judges[i].trueScore.toFixed(4)).to.eql(infit[i].toFixed(4));    
+          }          
+        }
+      });
+    });
+  
+});
--- test/statutils.spec.js
+++ test/statutils.spec.js
@@ -24,4 +24,35 @@ describe( 'statutils', function(){
         } );
     } );
   
-} );
+    describe('info', function(){
+      var info = statutils.info;
+      it('should return p * (1-p)',function(){
+          expect(info(0.5)).to.equal(0.25);
+          expect(info(0.3)).to.equal(0.21);
+          expect(info(0.9)).to.be.between(0.0899,0.09);
+      })
+    });  
+  
+    describe('zsq', function(){
+      var zsq = statutils.zsq;
+      it('should return res ^ 2 / info',function(){
+          expect(zsq(0.5)).to.equal(1);
+          expect(zsq(0.1)).to.equal(9);
+          expect(zsq(0.99)).to.be.between(0.01010101,0.01010102);
+      })
+    }); 
+  
+    describe('wms', function(){
+      var wms = statutils.wms;
+      var probs = [0.5,0.5,0.5,0.5];
+      var probs1 = [0.1,0.2,0.3,0.05];
+      var probs2 = [0.8,0.7,0.6,0.9];
+      it('should return sum info * zsq / sum info', function(){
+          expect(wms(probs)).to.equal(1);
+          expect(wms([])).to.equal(0);
+          expect(wms(probs1)).to.be.above(1);
+          expect(wms(probs2)).to.be.below(1);
+      })
+    });
+  
+});
